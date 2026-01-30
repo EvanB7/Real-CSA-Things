9954b475661cd9c8dc0255af8df6fc1030eb990d
EvanB7 <evanjbianco@gmail.com>
2026-01-30 07:51:10 -0500

    Add files via upload

---
+++ ArrayList Project/Demo.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * demonstrate basic arraylist features
+ *
+ * @author (evan and jinu)
+ * @version (1/28/26)
+ */
+public class Demo
+{
+    public static void main (String [] args) {
+            /*int [] data = {3, 4, 5, 6, 7};
+    
+            
+            ArrayList<Integer> data2 = new ArrayList<Integer>(Arrays.asList(3,4,5,6,7));
+    
+            // data2.add(3);
+            // data2.add(4);
+            // data2.add(5);
+            // data2.add(6);
+            // data2.add(7);
+            data2.set(0,9);
+            //[9,4,5,6,7];
+            data2.add(2,0);
+            data2.add(1);
+            
+            System.out.println("Hello World");
+    
+            for (int i=data2.size() - 1; i >= 0; i--){
+                System.out.print(data2.get(i));
+            }
+            System.out.println();
+        }
+        */
+   
+        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(7,7,1));
+    
+    }
+}
